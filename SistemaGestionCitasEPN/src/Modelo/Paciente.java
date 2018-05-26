035dec6c2f8d1d1948f79070218593344c778790
Dennis238 <dennis.238@hotmail.com>
2018-05-26 00:43:46 -0500

    Inicio

---
+++ SistemaGestionCitasEPN/src/Modelo/Paciente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelo;
+
+/**
+ *
+ * @author Alexis
+ */
+public class Paciente {
+    private String cedula;
+    private String nombreP;
+    private String nombreS;
+    private String apellidoP;
+    private String apellidoS;
+    private String telefonoP;    
+    private String telefonoS;
+    private String email;
+    private String fechaNacimiento;
+    private String direccion;
+}
